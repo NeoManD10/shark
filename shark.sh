1e5cff2a398965fc4886e3b91a8e7f884c8097a7
Manuel Dios <mdios@utem.cl>
2025-01-04 00:11:52 -0300

    commit

--- shark.sh
+++ shark.sh
@@ -12,32 +12,23 @@ PORT='8080'
 
 BASE_DIR=$(realpath "$(dirname "$BASH_SOURCE")") #Ruta absoluta del directorio donde esta el script
 
-if [[ ! -d ".server" ]]; 
+#Crear directorios al momento de la ejecucion
+
+if [[ ! -d ".servidor" ]]; 
 then
-	mkdir -p ".server"  #-p: Si el directorio ya existe, no arroja error y mantiene el mismo directorio
+    mkdir -p ".servidor"  #-p: Si el directorio ya existe, no arroja error y mantiene el mismo directorio
 fi
 
 if [[ ! -d "auth" ]]; 
 then
-	mkdir -p "auth"
+    mkdir -p "auth"
 fi
 
-trap exit_on_signal_SIGINT SIGINT
-trap exit_on_signal_SIGTERM SIGTERM
-
-check_PID="php cloudflared loclx"
-for process in ${check_PID}; do
-	if [[ $(pidof ${process}) ]]; then
-		killall ${process} > /dev/null 2>&1
-	fi
-done
-
-if [[ $(command -v php) && $(command -v curl) && $(command -v unzip) ]]; then
-	echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Dependencias instaladas."
+if [[ -d ".server/www"]];
+then
+    rm -rf ".server/www"
+	mkdir -p ".server/www"
 else
-	pkgs=(php curl unzip)
-	for pkg in "${pkgs[@]}"; do
-		...
-	done
+    mkdir -p ".server/www"
 fi
 
